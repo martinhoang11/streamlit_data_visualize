1e3afa2c186f7a44b42f8cbd239a26efa9363ff0
Nguyễn Hoàng Huy (VNCDLL-CTÐTKSAI) <huynh14@vingroup.net>
2021-02-05 22:40:54 +0700

    work with vn_bank data

--- test_st.py
+++ test_st.py
@@ -5,33 +5,35 @@ import matplotlib.pyplot as plt
 import altair as alt
 from matplotlib.pyplot import figure
 
-st.title('Fina data visualize')
-
-data_url = ('C:/Users/huynh14/fina/moigioi_clean.xlsx')
-
+#function
 @st.cache(allow_output_mutation=True)
-def load_data():
-    data = pd.read_excel(data_url)
+def load_data(url):
+    data = pd.read_excel(url)
     return data
 
-add_selectbox = st.selectbox(
+#sidebar settings
+st.sidebar.subheader('Configure Settings')
+add_selectbox = st.sidebar.selectbox(
     'How would you like to be contacted?',
     ('Email', 'Home phone', 'Mobile phone')
 )
 
 # Add a slider to the sidebar:
-show_range = add_slider = st.slider(
+show_range = st.sidebar.slider(
     'Select a range of values',
     0.0, 100.0, (25.0, 75.0)
 )
-st.write(f"- Range choose: {show_range}")
 
+
+#Moigioi
+st.title('Moi gioi data visualize')
+
+#data url
+data_url = ('C:/Users/huynh14/fina/moigioi_clean.xlsx')
 
 #load data
-data_load_state = st.text('Loading data...')
-data = load_data()
+data = load_data(data_url)
 data = data.iloc[:, 3:]
-data_load_state.text('Loading data...done!')
 
 #show data
 if st.checkbox('Show raw data'):
@@ -53,4 +55,20 @@ for phone in data['Email']:
         email_dict['Valid'] += 1
 
 st.bar_chart(pd.DataFrame(pd.Series(email_dict).to_frame()), width=1600,height=500)
-# st.set_option('deprecation.showPyplotGlobalUse', False) 
+
+#City visualize
+st.subheader('Top 10 cities for Moi Gioi: ')
+fig_website = data['City_clean'].value_counts().sort_values()[-10:]
+if st.checkbox('Show data'):
+    st.write(fig_website)
+st.bar_chart(fig_website, width=1600,height=500)
+
+
+#Vietnam_bank
+st.title('Vietnam Bank post data visualize')
+vn_bank_url = ('C:/Users/huynh14/fina/vietnam_bank_amc.xlsx')
+vn_bank_data = load_data(vn_bank_url)
+
+st.subheader('No. of post from website: ')
+fig_website = vn_bank_data['Website'].value_counts()
+st.bar_chart(fig_website, width=1600,height=500)
